4c3e5b7cde826f171cf1203d6d53fd3ac62e573c
USER01 <kho4514@naver.com>
2023-11-01 10:06:12 +0900

    .

--- app/src/main/java/com/example/giftimoa/Collect_gift_add_activity.kt
+++ app/src/main/java/com/example/giftimoa/Collect_gift_add_activity.kt
@@ -1,7 +1,10 @@
 package com.example.giftimoa
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.giftimoa.bottom_nav_fragment.Collect_fragment
 import com.example.giftimoa.databinding.LayoutCollectGiftAddBinding
 
 class Collect_gift_add_activity : AppCompatActivity() {
@@ -16,8 +19,21 @@ class Collect_gift_add_activity : AppCompatActivity() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.title = "기프티콘 등록"
     }
+    private fun CreateGiftCCollectBtn() {
+        binding.AddGiftBtn.setOnClickListener {
+            val textgiftiname: String = binding.textGiftName.text.toString()
+            val texteffectivedate = binding.textEffectiveDate.text.toString()
+            val textbarcode = binding.textBarcode.text.toString()
+            val textplace = binding.textPlace.text.toString()
 
-
+            val intent = Intent(this, Collect_fragment::class.java)
+            intent.putExtra("textgiftiname", textgiftiname)
+            intent.putExtra("texteffectivedate", texteffectivedate)
+            intent.putExtra("textbarcode", textbarcode)
+            intent.putExtra("textplace", textplace)
+            startActivity(intent)
+        }
+    }
     override fun onSupportNavigateUp(): Boolean {
         onBackPressed()
         return true
